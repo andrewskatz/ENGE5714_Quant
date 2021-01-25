a60fdd88a17d3026354777fb56bc99d251894c99
A Katz <akatz4@vt.edu>
2021-01-25 11:11:23 -0500

    Week 2 updates

--- Week 2/week_2_script.R
+++ Week 2/week_2_script.R
@@ -1,5 +1,16 @@
 ## Week 2 Notes
 
+## One of the first things you will have in any script or .rmd file is a section to load
+## all the libraries that you use in that script.
+
+# you can install a library by using the install.packages() function, for example:
+install.packages("tidyverse")
+
+# with this installed, you can then load the package using the library() function
+
+
+library(tidyverse)
+
 
 
 # Distributions
